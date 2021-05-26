d2fdf5a24cd71c299e22fcec7b681442d3415519
Ioan Andrei <ivan.andrei93@mail.ru>
2021-05-27 01:16:39 +0200

    made the scanner run properly and even found an open port.

--- main.py
+++ main.py
@@ -11,22 +11,24 @@ lowpoint = int(target_port.split("-")[0])
 highpoint = int(target_port.split("-")[1])
 
 #Create socket.
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Scanning for open ports.
 def scan_port():
     for port in range(lowpoint, highpoint):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         status = s.connect_ex((target_ip, port))
         if(status == 0):
-            print("Port ",port, " is OPEN.")
+            print("Port",port, "is OPEN.")
         else:
-            print("Port ",port," is CLOSED")
-    
-    answer = int(input("Type the port you would like to connect to: "))
-    if(answer == status):
-        print("Connecting...")
-    else:
-        print("Port is closed.")
+            print("Port",port,"is CLOSED")
+    s.close()
+
+scan_port()
+    # answer = int(input("Type the port you would like to connect to: "))
+    # if(answer == status):
+    #     print("Connecting...")
+    # else:
+    #     print("Port is closed.")
 
 
 #Scan for infomation about the machine and operating system
