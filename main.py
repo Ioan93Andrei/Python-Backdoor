ba8b274ca3c9b7e510384fd84e7414d56220287c
Ioan Andrei <ivan.andrei93@mail.ru>
2021-05-27 00:56:33 +0200

    some progress, may not work properly.

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ target_port = input("Please type in the target ports range (ex. 0 - 500): ")
 lowpoint = int(target_port.split("-")[0])
 highpoint = int(target_port.split("-")[1])
 
-#Scanning for open ports
-for port in range(lowpoint, highpoint):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    status = s.connect_ex((target_ip, port))
-    if(status == 0):
-        print("Port ",port, " is OPEN.")
+#Create socket.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Scanning for open ports.
+def scan_port():
+    for port in range(lowpoint, highpoint):
+        status = s.connect_ex((target_ip, port))
+        if(status == 0):
+            print("Port ",port, " is OPEN.")
+        else:
+            print("Port ",port," is CLOSED")
+    
+    answer = int(input("Type the port you would like to connect to: "))
+    if(answer == status):
+        print("Connecting...")
     else:
-        print("Port ",port," is CLOSED")
+        print("Port is closed.")
+
+
+#Scan for infomation about the machine and operating system
+
+#Browse the file system
