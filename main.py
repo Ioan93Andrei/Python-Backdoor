2096544fe6756c9071a54bff73bb851268f0a2ea
Ioan Andrei <ivan.andrei93@mail.ru>
2021-05-27 01:28:19 +0200

    moved the socket creation out of the scan_port function.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import socket
 import os
 import platform
+from functions import *
 
 #Getting the target IP addresse and the ports to scan.
 target_ip = input("Please type in the target IP address: ")
@@ -10,19 +11,18 @@ target_port = input("Please type in the target ports range (ex. 0 - 500): ")
 lowpoint = int(target_port.split("-")[0])
 highpoint = int(target_port.split("-")[1])
 
-#Create socket.
+#Create socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Scanning for open ports.
 def scan_port():
     for port in range(lowpoint, highpoint):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         status = s.connect_ex((target_ip, port))
         if(status == 0):
             print("Port",port, "is OPEN.")
         else:
             print("Port",port,"is CLOSED")
-    s.close()
-
+            
 scan_port()
     # answer = int(input("Type the port you would like to connect to: "))
     # if(answer == status):
